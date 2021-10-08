aa9b65d1918a36b2430f3f4ca1f720abca0e4e39
Petr Samoseyko <p.samoseiko@g.nsu.ru>
2021-10-08 20:17:04 +0700

    resolved (probably) gitignore conflicts

--- Task-1-1-2/src/test/java/nsu/fit/oop/substring/Tests.java
+++ Task-1-1-2/src/test/java/nsu/fit/oop/substring/Tests.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,41 +17,38 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Tests {
     @ParameterizedTest
-    @MethodSource ("searchTestGenerator")
+    @MethodSource("searchTestGenerator")
     public void testSearchNormal(String filename, String query, ArrayList<Integer> expected) throws IOException {
         File file = new File(filename);
-        FileReader r = new FileReader(file);
-        assertEquals (expected, Substring.search(r,query));
+        FileInputStream r = new FileInputStream(file);
+        assertEquals(expected, Substring.search(r, query));
     }
 
-    static Stream<Arguments> searchTestGenerator(){
-        ArrayList<Integer> expected1 = new ArrayList<>();
-        expected1.add(18);
-        expected1.add(43);
-        ArrayList<Integer> expected2 = new ArrayList<>();
-        expected2.add(7);
-        expected2.add(21);
-        ArrayList<Integer> expected3 = new ArrayList<>();
-        expected3.add(8192);
-        /*ArrayList<Integer> expected4 = new ArrayList<>();
-        expected4.add(7);*/
+    static Stream<Arguments> searchTestGenerator() {
+        ArrayList<Integer> expected1 = new ArrayList<>(List.of(18, 43));
+        ArrayList<Integer> expected2 = new ArrayList<>(List.of(7, 21));
+        ArrayList<Integer> expected3 = new ArrayList<>(List.of(8192));
+        ArrayList<Integer> expected4 = new ArrayList<>(List.of(7));
+        String pirog = new String("пирог".getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
         return Stream.of(
                 Arguments.of("input.txt", "re ", expected1),
                 Arguments.of("multiline.txt", "line", expected2),
-                Arguments.of("bigdata.txt", "s", expected3)/*,
-                Arguments.of("in.txt", "пирог",expected4)*/
+                Arguments.of("bigdata.txt", "s", expected3),
+                Arguments.of("in.txt", pirog,expected4)
         );
     }
+
     @ParameterizedTest
     @NullAndEmptySource
     public void testNullAndEmptyQuery(String query) throws FileNotFoundException {
-        BufferedReader r = new BufferedReader(new FileReader("input.txt"));
-        assertThrows(IllegalArgumentException.class, ()->Substring.search(r,query));
+        FileInputStream r = new FileInputStream("input.txt");
+        assertThrows(IllegalArgumentException.class, () -> Substring.search(r, query));
     }
+
     @ParameterizedTest
     @NullSource
-    public void testNullReader(Reader r) {
+    public void testNullReader(FileInputStream r) {
         String q = "s";
-        assertThrows(IllegalArgumentException.class, ()->Substring.search(r,q));
+        assertThrows(IllegalArgumentException.class, () -> Substring.search(r, q));
     }
 }
