0511866eb0d43d6d9d92469a15ea49a9adc83926
Petr Samoseyko <p.samoseiko@g.nsu.ru>
2021-10-28 11:24:07 +0700

    calculator first version

---
+++ Task-1-4-1/src/main/java/nsu/fit/oop/Calculator.java
@@ -0,0 +1,43 @@
+package nsu.fit.oop;
+
+import java.util.Scanner;
+
+//pow sqrt sin cos (log?)
+
+public class Calculator {
+    private static double parseExpression(Scanner s){
+        if (!s.hasNextInt()) {
+            String operation = s.next();
+            switch (operation) {
+                case "+":
+                    return parseExpression(s)+parseExpression(s);
+                case "-":
+                    return parseExpression(s)-parseExpression(s);
+                case "*":
+                    return parseExpression(s)*parseExpression(s);
+                case "/":
+                    return parseExpression(s)/parseExpression(s);
+                case "pow":
+                    return Math.pow(parseExpression(s),parseExpression(s));
+                case "sqrt":
+                    return Math.sqrt(parseExpression(s));
+                case "sin":
+                    return Math.sin(parseExpression(s));
+                case "cos":
+                    return Math.cos(parseExpression(s));
+                case "log":
+                    return Math.log(parseExpression(s));
+            }
+        }
+        else {
+            return s.nextDouble();
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double res = parseExpression(s);
+        System.out.println(res);
+    }
+}
