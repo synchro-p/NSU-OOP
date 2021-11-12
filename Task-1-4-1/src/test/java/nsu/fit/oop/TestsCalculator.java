5e8bc3d45983bd787496c672c597835e61dded7e
Petr Samoseyko <p.samoseiko@g.nsu.ru>
2021-11-12 15:09:59 +0700

    added working tests

---
+++ Task-1-4-1/src/test/java/nsu/fit/oop/TestsCalculator.java
@@ -0,0 +1,28 @@
+package nsu.fit.oop;
+
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestsCalculator {
+    private static Stream<Arguments> getPresetExpressions(){
+        return Stream.of(
+                Arguments.of("sin + - 1 2 1", 0),
+                Arguments.of("pow 2 5", 32),
+                Arguments.of("cos * 3,1415926535 + 0,6 0,4", -1.0),
+                Arguments.of("* 25 6",150)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getPresetExpressions")
+    public void testExpressions(String expr, double expected){
+        assertEquals(expected,Calculator.parseExpression(new Scanner(expr)));
+    }
+}
