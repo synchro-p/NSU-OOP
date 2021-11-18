303f8a84c7ac059f4145a54901fba179d59c53f1
Petr Samoseyko <p.samoseiko@g.nsu.ru>
2021-11-18 18:14:57 +0700

    notebook first version

---
+++ Task-1-4-2/src/main/java/nsu/fit/oop/Entry.java
@@ -0,0 +1,47 @@
+package nsu.fit.oop;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import java.time.LocalDateTime;
+
+public class Entry {
+    @JsonSerialize(using = StringSerializer.class)
+    @JsonDeserialize(using = StringDeserializer.class)
+    private final String name;
+    @JsonSerialize(using = StringSerializer.class)
+    @JsonDeserialize(using = StringDeserializer.class)
+    private final String content;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private final LocalDateTime creationDateTime;
+
+    Entry() {
+        name = "";
+        content = "";
+        creationDateTime = null;
+    }
+
+    Entry(String n, String c) {
+        name = n;
+        content = c;
+        creationDateTime = LocalDateTime.now();
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    String getContent() {
+        return this.content;
+    }
+
+    LocalDateTime getCreationDateTime() {
+        return this.creationDateTime;
+    }
+}
