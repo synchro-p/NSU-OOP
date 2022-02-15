ff8e25d8c6839dd006362112d97a092ade2e387d
Petr Samoseyko <p.samoseiko@g.nsu.ru>
2022-02-15 17:52:23 +0700

    added renderer and reworks

---
+++ Task-2-1-1/src/main/java/Drawer.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Drawer extends JPanel{
+    private int[] yys = null;
+    private int HEIGHT = 720;
+    private int WIDTH = 1080;
+    private final int scale = 4;
+    public void setHEIGHT(Integer newHeight) {
+        HEIGHT = newHeight;
+    }
+
+    public void setWIDTH(Integer newWidth) {
+        WIDTH = newWidth;
+    }
+
+    public void setYs(ArrayList<Double> points) {
+        points = points.stream().mapToDouble(a -> a * HEIGHT).boxed().collect(Collectors.toCollection(ArrayList::new));
+        yys = points.stream().mapToInt(a -> HEIGHT-a.intValue()/scale).toArray();
+    }
+
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        this.setBackground(Color.WHITE);
+        graphics.setColor(Color.BLUE);
+        int[] xs={WIDTH/6-WIDTH/12,WIDTH*2/6-WIDTH/12,WIDTH*3/6-WIDTH/12,WIDTH*4/6-WIDTH/12,WIDTH*5/6-WIDTH/12,WIDTH-WIDTH/12};
+        System.out.println(Arrays.toString(xs) +"\n"+ Arrays.toString(yys));
+        graphics.drawPolyline(xs,yys,6);
+    }
+}
